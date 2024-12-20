2b6fb1d669a7f8a50a8319f84ef0946013da69e2
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-12-20 21:29:19 +0900

    fix

--- src/lib/NostrShare.svelte
+++ src/lib/NostrShare.svelte
@@ -4,7 +4,21 @@
   import Dialog from "./Dialog.svelte";
   import { nostrIcon } from "./icons";
 
-  let { url, text, shareTitle, icon = false, ...props } = $props();
+  export interface NostrShareProps {
+    shareTitle?: string;
+    url?: string;
+    text?: string;
+    icon?: boolean;
+    class?: string;
+  }
+
+  let {
+    url,
+    text,
+    shareTitle,
+    icon = false,
+    ...props
+  }: NostrShareProps = $props();
   //https://zenn.dev/tnzk/articles/835d3252ce01ed#dom-api%E7%B5%8C%E7%94%B1%E3%81%AE%E5%B1%9E%E6%80%A7%E7%9B%B4%E3%81%AE%E5%A4%89%E6%9B%B4%E3%81%AF%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88%E3%81%95%E3%82%8C%E3%81%A6%E3%81%84%E3%82%8B%E3%81%8C%E3%80%81bind-%E6%A9%9F%E6%A7%8B%E3%81%AF%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88%E3%81%95%E3%82%8C%E3%81%A6%E3%81%AA%E3%81%84
   //let dialog: HTMLDialogElement;
   let openDialog = $state(false);
