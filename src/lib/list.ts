8a54180bce1acdaaa89a796a6d029a55a97a5944
TsukemonoGit <mine43try@outlook.com>
2025-05-20 12:49:02 +0900

    リスト50温順

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -1,5 +1,10 @@
 export const list = [
   {
+    name: "Nos Haiku",
+    url: "https://nos-haiku.vercel.app/antenna?content={text}",
+    icon: "https://nos-haiku.vercel.app/favicon.png",
+  },
+  {
     name: "nostter",
     url: "https://nostter.app/post?content={text}",
     icon: "https://nostter.app/favicon.ico",
@@ -8,10 +13,5 @@ export const list = [
     name: "Lumilumi",
     url: "https://lumilumi.app/post?text={text}",
     icon: "https://lumilumi.app/favicon.ico",
-  }, 
-  {
-    name: "Nos Haiku",
-    url: "https://nos-haiku.vercel.app/antenna?content={text}",
-    icon: "https://nos-haiku.vercel.app/favicon.png",
   },
 ];
